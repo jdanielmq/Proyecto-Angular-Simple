326598156ce4fce0602177fc31f72ba0d167d0a5
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-10-07 10:06:03 -0300

    aplica la coneccion a la api restfull de spring-boot ambos proyecto estan sin erroress y se levantan y conectan sin problemas

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,14 +2,22 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente'
 import { CLIENTES } from './clientes.json';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
-   getClientes(): Observable<Cliente[]> {
-     return of(CLIENTES);
-   }
+  private urlEndpoint: string = "http://localhost:9090/api/clientes";
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+     //return of(CLIENTES);
+     return this.http.get(this.urlEndpoint).pipe(
+       map( (response) => response as Cliente[])
+     );
+  }
 }
