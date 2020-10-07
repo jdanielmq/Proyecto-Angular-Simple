d206009447cd20d3844d66b652991532b1b4ee6b
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-10-07 07:56:38 -0300

    proyecto de angular simple

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id:1 , nombre:'Juan Daniel', apellido:' Queupul', email:'jjjjjjj@gmail.com', createAt:'2020-02-02'},
+  {id:2 , nombre:'Daniel', apellido:'Muñoz ', email:'aaaaaaa@gmail.com', createAt:'2020-02-02'},
+  {id:3 , nombre:'Juan', apellido:' Queupul', email:'ddddddd@gmail.com', createAt:'2020-02-02'},
+  {id:4 , nombre:'jose Daniel', apellido:' Queupul', email:'tttttt@gmail.com', createAt:'2020-02-02'},
+  {id:5 , nombre:'lucas Daniel', apellido:' Queupul', email:'dddddddd@gmail.com', createAt:'2020-02-02'},
+  {id:6 , nombre:'fabricio Daniel', apellido:' Queupul', email:'fffffff@gmail.com', createAt:'2020-02-02'},
+  {id:7 , nombre:'loco Daniel', apellido:' Queupul', email:'ffffff@gmail.com', createAt:'2020-02-02'},
+  {id:8 , nombre:'maestro Daniel', apellido:'Muñoz ', email:'tttttttt@gmail.com', createAt:'2020-02-02'},
+  {id:9 , nombre:'maestro Daniel', apellido:'Muñoz ', email:'tttttttt@gmail.com', createAt:'2020-02-02'}
+];
